e3f1af8be3da2cf3b270c028b7a13063d52b4879
kros <ormartin@gmail.com>
2015-05-21 15:58:35 +0200

    Basic functionality

--- src/Logger.php
+++ src/Logger.php
@@ -8,41 +8,109 @@ namespace Kros\Logger;
  */
 class Logger{
 	private $fileName="";
-	private $defaultFileName='default-#date{Ymd}#.log';
-	private $defaultLogFormat='[#date{Y-m-d H:i:s}#]\t[#level#]\t[#server{PHP_SELF}#\t#text#]';
+	private $fileNameFormat="";
+	private $defaultFileNameFormat='default-#date{Ymd}#.log';
+	private $logFormat=null;
+	private $defaultLogFormat="[#date{Y-m-d H:i:s}#]\t[#level#]\t[#server{PHP_SELF}#]\t#text#";
+	private $updateFileName;
+	private $minLogLevel=null;
+	private $defaultMinLogLevel="ERR";
+	private $defaultLogLevel="ERR";
 	
-	public function __construct($fileName=NULL){
-		if ($fileName==null){
-			$this->fileName=$fileName;
+	public function __construct($fileNameFormat=NULL, $updateFileName=FALSE){
+		$this->updateFileName=$updateFileName;
+		if ($fileNameFormat!=NULL){
+			$this->fileNameFormat=$fileNameFormat;
 		}else{
-			$this->fileName=$this->defaultFileName;
+			$this->fileNameFormat=$this->defaultFileNameFormat;
 		}
+		$this->loadLogFileName();
 	}
 	
-	private function composeFileName(){
+	public function setLogFormat($format){
+		$this->logFormat=$format;
+		return $this;
+	}
+	
+	public function getLogFormat(){
+		if($this->logFormat==null){
+			$this->logFormat=$this->defaultLogFormat;
+		}
+		return $this->logFormat;
+	}
+	
+	public function setDevaultLogLevel($level){
+		$this->defaultLogLevel=$level;
+		return $this;
+	}
+	
+	public function getDefaultLogLevel(){
+		return $this->defaultLogLevel;
+	}
+	
+	public function setMinLogLevel($level){
+		$this->minLogLevel=$level;
+		return $this;
+	}
+	
+	public function getMinLogLevel(){
+		if($this->minLogLevel==null){
+			$this->minLogLevel=$this->defaultMinLogLevel;
+		}
+		return $this->minLogLevel;
+	}
+	
+	public function getFileName(){
+		return $this->fileName;
+	}
+	
+	private function composeString($format, $text='', $level=''){
+		$res=$format;
+
+		//date token
+		$pattern='/#date{([\w:\s\/.+\-]*)}#/';
+		while (preg_match_all($pattern, $res, $out)) {
+			$res=str_replace($out[0][0], date($out[1][0]), $res);
+		}
+
+		//server token
+		$pattern='/#server{([\w]*)}#/';
+		while (preg_match_all($pattern, $res, $out)){
+			$res=str_replace($out[0][0], $_SERVER[$out[1][0]], $res);
+		}
+
+		//text token
+		$pattern='/#text#/';
+		if (preg_match_all($pattern, $res, $out)){
+			$res=str_replace($out[0][0], $text, $res);
+		}
 		
+		//level token
+			$pattern='/#level#/';
+		while (preg_match_all($pattern, $res, $out)){
+			$res=str_replace($out[0][0], $level, $res);
+		}
+		var_dump($res);
+		return $res;		
 	}
 	
-	private static function loadLogFileName(){
-		$p = ProjectProperties::getInstance();
-		if (isset($p->logFile))
-			self::$fileName=$p->logFile;
-		else
-			self::$fileName=self::$defaulFileName;
+	private function loadLogFileName(){
+		$this->fileName=$this->composeString($this->fileNameFormat);
 	}
-	public static function setLogFileName($name){
-		if ($name!=''){
-			self::$fileName=$name;
-		}else{
-			throw new Exception('Wrong log file name');
+	
+	public function log($logText, $level=NULL){
+		if ($this->updateFileName){
+			$this->loadLogFileName();
 		}
-	}
-	public static function log($logText){
-		if (self::$fileName=="")
-			self::loadLogFileName();
-		$handler = fopen(self::$fileName, 'a');
+		
+		if($level==null){
+			$level=$this->getDefaultLogLevel();
+		}
+		
+		$handler = fopen($this->fileName, 'a');
 		if (flock($handler, LOCK_EX)){
-			fwrite($handler, date('[Y-m-d H:i:s]' )."\t[".$_SERVER['PHP_SELF']."]\t".$logText."\n");
+			//TODO compare $level with $this->logLevel
+			fwrite($handler, $this->composeString($this->getLogFormat(),$logText, $level)."\n");
 			flock($handler, LOCK_UN);
 		}
 		fclose($handler);
